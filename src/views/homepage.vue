5a3631a3fe50c63ac4116f22e20cb9b2c6264915
Ben Graham <benben1510@gmail.com>
2020-11-25 17:17:04 +0200

    updated structure

--- src/views/homepage.vue
+++ src/views/homepage.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="homepage">
+   
+<h2>homepage</h2>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
+
 
 export default {
-  name: "Home",
+  name: "homepage",
   components: {
-    HelloWorld
+    
   }
 };
 </script>
