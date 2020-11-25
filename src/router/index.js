744d01ffba124c893a5e6fbe534b33a43dd6e640
dorgrosman <ddorgrosman@gmail.com>
2020-11-25 14:55:22 +0200

    15:00 --25/11/2020

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import VueRouter from "vue-router";
 import home from "../views/homepage.vue";
 import about from "../views/about.vue"
 import tripList from "../cmps/trip/trip-list.vue"
+import tripDetails from "../views/trip-details.vue"
+import userDetails from "../views/user-details.vue"
+import guideDetails from "../views/guide-details.vue"
+import backOffice from "../views/back-office.vue"
 
 Vue.use(VueRouter);
 
@@ -13,14 +17,29 @@ const routes = [
     component: home
   },
   {
-    path: "/",
-    name: "home",
-    component: home
+    path: "/user/:id",
+    name: "user-details",
+    component: userDetails
   },
   {
     path: "/trip",
     name: "trip-list",
     component: tripList
+  },
+  {
+    path: "/trip/:id",
+    name: "trip-details",
+    component: tripDetails
+  },
+  {
+    path: "/guide",
+    name: "guide-details",
+    component: guideDetails
+  },
+  {
+    path: "/back-office",
+    name: "back-office",
+    component: backOffice
   },
   {
     path: "/about",
