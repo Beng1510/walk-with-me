04224dd005dc71518282dbb2a7a9fbd206591ccd
Gal Ovadia <galov90@gmail.com>
2020-11-25 19:47:33 +0200

    1945 startet the trip preview cmp

---
+++ src/cmps/trip/trip-preview.cmp.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="trip-preview">
+      <img :src="`${trip.imgUrls[0]}`" alt="">
+      <h1>{{trip.name}}</h1>
+      <h3>{{trip.date}}</h3>
+      <p>{{trip.guide.name}}</p>
+      <p>{{trip.guide.rate}}</p>
+      <p>{{trip.guid.difficulty}}</p>
+      <p>{{booked}}/5 trippers</p>
+  </section>
+</template>
+
+<script>
+export default {
+    props: {
+        trip: {
+            type: Object
+        },
+        booked: {
+            type: Number
+        }
+    },
+
+    name: 'trip-preview',
+
+    methods: {
+       emitFav(id) {
+           this.$emit('emitFav', id);
+       } 
+    },
+
+    computed: {
+        // rateStars() {
+        //     let rate = trip.guide.rate;
+        //     let starsStr = '';
+        //     for (let i = 0; i < rate; i++) {
+        //         starsStr += '<i class="fas fa-star"></i>';
+        //     };
+        //     return starsStr;
+        // }
+    }
+}
+</script>
+
+<style>
+
+</style>
