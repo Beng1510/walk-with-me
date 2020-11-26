69d4177e8ece1a241a773439a490b34231b91529
Ben Graham <benben1510@gmail.com>
2020-11-26 18:21:29 +0200

    trips for disp and booking store

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,8 +1,14 @@
 import { httpService } from './http-service.js'
+const baseUrl = 'http://localhost:3000';
+
 
 export const bookingService = {
-
+    createBooking
   
 }
 
 
+function createBooking(booking) {
+   const res = axios.post(`${baseUrl}/booking`,booking)
+   return res.data
+}
