8bf7ba66e6713e370f121de76dfd4fb137aa65b6
Ben Graham <benben1510@gmail.com>
2020-11-28 12:49:15 +0200

    google maps

--- src/views/trip-details.vue
+++ src/views/trip-details.vue
@@ -2,11 +2,31 @@
   <section v-if="trip" class="trip-details">
     <button class="back-btn"><router-link to="/">Back</router-link></button>
     <h2>trip Details</h2>
-    <img class="trip-main-img" :src="require('../assets/img/trips/' + trip.imgUrls[0]) " />
-    <!-- <img class="hero" src="../assets/img/trips/scotland1.jpeg" /> -->
+    <div class="trip-detials-header flex space-between">
+      <div class="trip-details-img-container">
+        <img
+          class="trip-main-img"
+          :src="require('../assets/img/trips/' + trip.imgUrls[0])"
+        />
+      </div>
 
-    <!-- <img  class="tripImg" :src="trip.imgUrls[1]" alt="Image..." /> -->
-    <!-- <img  class="tripImg" :src="trip.imgUrls[2]" alt="Image..." /> -->
+      <div class="trip-details-map">
+        <GmapMap
+          :center="mapPos"
+          :zoom="12"
+          map-type-id="terrain"
+          style="width: 500px; height: 300px"
+        >
+          <GmapMarker
+            :position="mapPos"
+            :clickable="true"
+            :draggable="true"
+            @click="center = mapPos"
+          />
+        </GmapMap>
+      </div>
+    </div>
+
     <h2>Name: {{ trip.name }}</h2>
     <!-- <h3>Type: {{ trip.type }}</h3> -->
     <h3>Trip date: {{ trip.date }}</h3>
@@ -25,7 +45,6 @@
     <img class="guidImg" :src="trip.aboutGuide.imgUrl" alt="Image..." />
 
     <guide-review :guideId="trip.aboutGuide._id" />
-   
   </section>
 </template>
 
@@ -35,26 +54,27 @@ import guideReview from "../cmps/review/guide-review.cmp.vue";
 import tripBook from "../cmps/trip/trip-book.cmp.vue";
 
 export default {
-	data() {
-		return {
-			trip: null,            
-		};
-	},
-	
-	methods: {
-		bookTrip(booking) {
-			this.$store.dispatch({type:'addBooking', booking});
-		}
-	},
-	async created() { 
-		const tripId = this.$route.params.id;
-		const trip = await tripService.getTripById(tripId);
-		this.trip = trip;
-	},
-	components: {
-		guideReview,
-	
-		tripBook
-	},
+  data() {
+    return {
+      trip: null,
+      mapPos: null,
+    };
+  },
+
+  methods: {
+    bookTrip(booking) {
+      this.$store.dispatch({ type: "addBooking", booking });
+    },
+  },
+  async created() {
+    const tripId = this.$route.params.id;
+    const trip = await tripService.getTripById(tripId);
+    this.trip = trip;
+    this.mapPos = this.trip.latlang;
+  },
+  components: {
+    guideReview,
+    tripBook,
+  },
 };
 </script>
