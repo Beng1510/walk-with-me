9d82965b73fc1dbeeeeae9fce930150810e14380
Ben Graham <benben1510@gmail.com>
2020-12-07 19:28:05 +0200

    before merge

--- src/cmps/guide/guide-preview.cmp.vue
+++ src/cmps/guide/guide-preview.cmp.vue
@@ -1,12 +1,22 @@
 <template>
-  <section v-if="guide" class="guide-preview preview-card" @click="goToGuide(guide._id)">
+  <section
+    v-if="guide"
+    class="guide-preview preview-card"
+    @click="goToGuide(guide._id)"
+  >
     <img
       class="profile-img-m"
       :src="require('@/assets/img/users/' + guide.profileImgUrl)"
     />
     <div class="guide-info">
       <h2>{{ guide.name }}</h2>
-      <p><i class="fas fa-star trip-star-rate"></i> {{ rateGuide }} ({{rateAmount}}) </p>
+      <p>
+        <i class="fas fa-star trip-star-rate"></i> {{ rateGuide }} ({{
+          guide.guideInfo.totalReviewCount
+        }})
+
+        <!-- ({{rateAmount}})  -->
+      </p>
     </div>
   </section>
 </template>
@@ -23,6 +33,7 @@ export default {
   data() {
     return {
       totalRates: 0,
+      rateTotal: null,
     };
   },
   computed: {
@@ -36,14 +47,30 @@ export default {
       return avg.toFixed(1);
     },
     rateAmount() {
-      return this.guide.guideInfo.reviews.length
-    }
+      return this.guide.guideInfo.reviews.length;
+    },
   },
 
   methods: {
     goToGuide(id) {
       this.$router.push(`/guide/${id}`);
     },
+    // async getRateTotal(guide) {
+    //   console.log('guide',guide);
+    //   const guideId = guide._id;
+    //   console.log("🚀 ~ file: guide-preview.cmp.vue ~ line 61 ~ getRateTotal ~ guideId", guideId)
+    //   const total = await this.$store.dispatch({
+    //     type: "getRateTotal",
+    //     guideId,
+    //   });
+    //   console.log('total',total);
+    //   this.rateTotal = total;
+    //   console.log("🚀 ~ file: guide-preview.cmp.vue ~ line 66 ~ getRateTotal ~  this.rateTotal",  this.rateTotal)
+    // },
+  },
+  created() {
+    // console.log('this.guide',this.guide);
+    // this.getRateTotal(this.guide);
   },
 };
 </script>
