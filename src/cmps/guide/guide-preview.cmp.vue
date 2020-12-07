aeae1a1ca6db4e2ad0654f0f02e277bd7d505cde
Ben Graham <benben1510@gmail.com>
2020-12-07 19:28:48 +0200

    before again

--- src/cmps/guide/guide-preview.cmp.vue
+++ src/cmps/guide/guide-preview.cmp.vue
@@ -15,7 +15,6 @@
           guide.guideInfo.totalReviewCount
         }})
 
-        <!-- ({{rateAmount}})  -->
       </p>
     </div>
   </section>
@@ -43,35 +42,18 @@ export default {
         0
       );
       const avg = sum / this.guide.guideInfo.reviews.length;
-      // this.user.rate = this.avg
       return avg.toFixed(1);
     },
     rateAmount() {
       return this.guide.guideInfo.reviews.length;
     },
   },
-
   methods: {
     goToGuide(id) {
       this.$router.push(`/guide/${id}`);
     },
-    // async getRateTotal(guide) {
-    //   console.log('guide',guide);
-    //   const guideId = guide._id;
-    //   console.log("🚀 ~ file: guide-preview.cmp.vue ~ line 61 ~ getRateTotal ~ guideId", guideId)
-    //   const total = await this.$store.dispatch({
-    //     type: "getRateTotal",
-    //     guideId,
-    //   });
-    //   console.log('total',total);
-    //   this.rateTotal = total;
-    //   console.log("🚀 ~ file: guide-preview.cmp.vue ~ line 66 ~ getRateTotal ~  this.rateTotal",  this.rateTotal)
-    // },
   },
-  created() {
-    // console.log('this.guide',this.guide);
-    // this.getRateTotal(this.guide);
-  },
+ 
 };
 </script>
 
